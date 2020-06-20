e3c85c0a366b1c663b9bc45e99bbc0c0d8057ae8
Talal <tahmed530@gmail.com>
2020-06-20 15:57:13 +0500

    called Person tag 3 times to see random age

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>Assalam o Alaikum! This is a React App.</h1>
+        <Person />
+        <Person />
         <Person />
       </div>
     );
