4228a9787547ec20e808f9a862e2fb4d3cf5cabe
Talal <tahmed530@gmail.com>
2020-08-31 16:12:06 +0500

    checks using PureComponent

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Person from './Person/Person';
 import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 
-class Persons extends Component {
+class Persons extends PureComponent {
 
 	// static getDerivedStateFromProps(props, state) {
 	// 	console.log('[Persons.js] getDerivedStateFromProps', props);
 	// 	return state;
 	// }
 
-	shouldComponentUpdate(nextProps, nextState) {
-	    console.log('[Persons.js] shouldComponentUpdate');
-	    if (nextProps.persons !== this.props.persons ||
-	    	nextProps.clicked !== this.props.clicked ||
-	    	nextProps.nameChanged !== this.props.nameChanged) {
-	    	return true;
-	    }
-	    else {
-	    	return false;
-	    }
-	}
+	// shouldComponentUpdate(nextProps, nextState) {
+	//     console.log('[Persons.js] shouldComponentUpdate');
+	//     if (nextProps.persons !== this.props.persons ||
+	//     	nextProps.clicked !== this.props.clicked ||
+	//     	nextProps.nameChanged !== this.props.nameChanged) {
+	//     	return true;
+	//     }
+	//     else {
+	//     	return false;
+	//     }
+	// }
 
 	getSnapshotBeforeUpdate(prevProps, prevState) {
 		console.log('[Persons.js] getSnapshotBeforeUpdate');
