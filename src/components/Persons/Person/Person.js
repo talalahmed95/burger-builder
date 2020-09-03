5c00d5b4275e4bf3cd435a893c93b38b85b6c74e
Talal <tahmed530@gmail.com>
2020-09-03 21:41:40 +0500

    using refs: old way

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,13 +5,17 @@ import withClass from '../../../hoc/withClass';
 import PropTypes from 'prop-types';
 
 class Person extends Component {
+	componentDidMount() {
+		this.inputElement.focus();
+	}
+
 	render() {
 		console.log('[Person.js] rendering...');
 		return (
 			<Aux>
 				<p onClick={ this.props.click }>Hello! I am { this.props.name }. I am { this.props.age } years old.</p>
 				<p>{ this.props.children }</p>
-				<input type="text" onChange={this.props.changed} value={this.props.name} />
+				<input ref={(inputEl) => {this.inputElement = inputEl}} type="text" onChange={this.props.changed} value={this.props.name} />
 			</Aux>
 		);
 	}	
